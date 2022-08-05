753941928466b44e5b4704fec68fd85014c7f114
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-08-05 17:18:44 +0200

    gestion des listes terminée Les explication dans le fichier Listes.js

--- src/pages/Listes.js
+++ src/pages/Listes.js
@@ -1,9 +1,36 @@
 import { useState } from "react";
 
+// states (état, données)
 const Listes = () => {
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Cerise" }
+  ]);
+
+  // Comportements
+  const handleDelete = (id) => {
+    console.log(id);
+    // 1. Copie du state
+    const fruitsCopy = [...fruits];
+    // 2. Manipuler mon state
+    const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+    // 3. modifier mon state avec le setter
+    setFruits(fruitsCopyUpdated);
+  };
+
+  // affichage(render)
   return (
     <div className="liste">
       <h1>Listes</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            {fruit.nom}{" "}
+            <button onClick={() => handleDelete(fruit.id)}>x</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
